c1fa3c8532c2d81e1e04dbc48dacd5f13cc9eaf7
Debobrata Chakraborty <61970194+dckakon@users.noreply.github.com>
2025-08-09 14:36:00 +0600

    For loop

---
+++ LoopExp.java
@@ -0,0 +1,18 @@
+public class LoopExp{
+	
+    public static void main(String[] args) {
+        // Loop to print numbers from 10 to 0
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+
+	}
+
+} 
+
+
+
+
+
+
